d8fb92d547487e73a32eddddbb1d6eb3e31e0ff0
gameschool-student2 <stud2.gsch124@outlook.com>
2025-10-01 18:06:50 +0700

    Update Artem.cpp

--- Artem.cpp
+++ Artem.cpp
@@ -28,63 +28,57 @@ struct boxl2 {
 };
 
 obj racket, ball;
-boxl1 box11, box12, box13, box14, box15, box16, box17, box18, box21, box22, box23, box24, box25, box26, box27, box31, box32, box33, box34,
-box35, box36;
-boxl2 box41, box42, box43, box44, box45, box46, box47, box51, box52, box53, box54, box55, box56, box61, box62, box63, box64, box65;
-boxl1 boxes1[]{ box11, box12, box13, box14, box15, box16, box17, box18, box21, box22, box23, box24, box25, box26, box27, box31, box32, box33, box34,
-box35, box36 };
-boxl2 boxes2[]{ box41, box42, box43, box44, box45, box46, box47, box51, box52, box53, box54, box55, box56, box61, box62, box63, box64, box65 };
+//boxl1 box11, box12, box13, box14, box15, box16, box17, box18, box21, box22, box23, box24, box25, box26, box27, box31, box32, box33, box34,
+//box35, box36;
+//boxl2 box41, box42, box43, box44, box45, box46, box47, box51, box52, box53, box54, box55, box56, box61, box62, box63, box64, box65;
+boxl1 boxes1[21]{};
+boxl2 boxes2[18]{};
 int lives = 3;
 int score = 0;
 int level = 1;
 HBITMAP hBack;
 
 void InitGame() {
+
 	racket.x = window.height / 2 - 75;
 	racket.y = window.height - 30;
 	racket.width = 150;
 	racket.height = 30;
 	racket.speed = 10;
-	ball.x = window.width / 2 - 50;
-	ball.y = window.height - 130;
+	ball.x = window.width / 2;
+	ball.y = window.height/2;
 	ball.width = ball.height = 100;
 	ball.speed = 8;
 	racket.hBitmap = (HBITMAP)LoadImageA(NULL, "racket.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
 	ball.hBitmap = (HBITMAP)LoadImageA(NULL, "ball.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-
-	box11.y = box12.y = box13.y = box14.y = box15.y = box16.y = box17.y = box18.y = 50;
-	box21.y = box22.y = box23.y = box24.y = box25.y = box26.y = box27.y = 50 + window.height / 8;
-	box31.y = box32.y = box33.y = box34.y = box35.y = box36.y = 50 + window.height / 4;
-	box41.y = box42.y = box43.y = box44.y = box45.y = box46.y = box47.y = -200 - window.height / 4;
-	box51.y = box52.y = box53.y = box54.y = box55.y = box56.y = -200 - window.height / 8;
-	box61.y = box62.y = box63.y = box64.y = box65.y = -200;
-	box11.x = box41.x = 0;
-	box12.x = box31.x = window.width / 8;
-	box13.x = box32.x = window.width / 4;
-	box14.x = box33.x = window.width / 8 * 3;
-	box15.x = box34.x = window.width / 2;
-	box16.x = box35.x = window.width / 8 * 5;
-	box17.x = box36.x = window.width / 8 * 6;
-	box18.x = window.width / 8 * 7;
-	box21.x = window.width / 16;
-	box22.x = window.width / 16 * 3;
-	box23.x = window.width / 16 * 5;
-	box24.x = window.width / 16 * 7;
-	box25.x = window.width / 16 * 9;
-	box26.x = window.width / 16 * 11;
-	box27.x = window.width / 16 * 13;
-	box42.x = box61.x = window.width / 7;
-	box43.x = box62.x = window.width / 7 * 2;
-	box44.x = box63.x = window.width / 7 * 3;
-	box45.x = box64.x = window.width / 7 * 4;
-	box46.x = box65.x = window.width / 7 * 5;
-	box47.x = window.width / 7 * 6;
-	box51.x = window.width / 14;
-	box52.x = window.width / 14 * 3;
-	box53.x = window.width / 14 * 5;
-	box54.x = window.width / 14 * 7;
-	box55.x = window.width / 14 * 9;
-	box56.x = window.width / 14 * 11;
+	for (int i = 0; i < 21; i++) {
+		if (i < 8) {
+			boxes1[i].x = window.width / 8 * i;
+			boxes1[i].y = 50;
+		}
+		else if (i < 15) {
+			boxes1[i].x = window.width / 8 * (i - 8) + window.height / 16;
+			boxes1[i].y = 50 + window.height / 8;
+		}
+		else {
+			boxes1[i].x = window.width / 8 * (i - 16);
+			boxes1[i].y = 50 + window.height / 4;
+		}
+	}
+	for (int i = 0; i < 18; i++) {
+		if (i < 7) {
+			boxes2[i].x = window.width / 7 * i;
+			boxes2[i].y = -200 - window.height / 4;
+		}
+		else if (i < 13) {
+			boxes2[i].x = window.width / 7 * (i - 8) + window.height / 14;
+			boxes2[i].y = -200 - window.height / 8;
+		}
+		else {
+			boxes2[i].x = window.width / 7 * (i - 16);
+			boxes2[i].y = -200;
+		}
+	}
 	hBack = (HBITMAP)LoadImageA(NULL, "jungle.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
 }
 
@@ -93,6 +87,20 @@ void setText() {
 	SetBkMode(window.context, TRANSPARENT);
 	auto hFont = CreateFont(50, 0, 0, 0, FW_BOLD, 0, 0, 0, 0, 0, 0, 10, 0, L"CALIBRI");
 	auto hTmp = (HFONT)SelectObject(window.context, hFont);
+}
+
+void InitWindow() {
+	SetProcessDPIAware();
+	window.hWnd = CreateWindow(L"edit", 0, WS_POPUP | WS_VISIBLE | WS_MAXIMIZE, 0, 0, 0, 0, 0, 0, 0, 0);
+
+	RECT r;
+	GetClientRect(window.hWnd, &r);
+	window.device_context = GetDC(window.hWnd);
+	window.width = r.right - r.left;
+	window.height = r.bottom - r.top;
+	window.context = CreateCompatibleDC(window.device_context);
+	SelectObject(window.context, CreateCompatibleBitmap(window.device_context, window.width, window.height));
+	GetClientRect(window.hWnd, &r);
 }
 
 void ShowBitmap(HDC hDC, int x, int y, int x1, int y1, HBITMAP hBitmapBall, bool alpha = false)
@@ -110,20 +118,6 @@ void ShowBitmap(HDC hDC, int x, int y, int x1, int y1, HBITMAP hBitmapBall, bool
 		SelectObject(hMemDC, hOldbm);
 	}
 	DeleteDC(hMemDC);
-}
-
-void InitWindow() {
-	SetProcessDPIAware();
-	window.hWnd = CreateWindow(L"edit", 0, WS_POPUP | WS_VISIBLE | WS_MAXIMIZE, 0, 0, 0, 0, 0, 0, 0, 0);
-
-	RECT r;
-	GetClientRect(window.hWnd, &r);
-	window.device_context = GetDC(window.hWnd);
-	window.width = r.right - r.left;
-	window.height = r.bottom - r.top;
-	window.context = CreateCompatibleDC(window.device_context);
-	SelectObject(window.context, CreateCompatibleBitmap(window.device_context, window.width, window.height));
-	GetClientRect(window.hWnd, &r);
 }
 
 void ballMove() {
@@ -226,7 +220,7 @@ void newLevel() {
 	if (hit == 21) {
 		ball.x = window.width / 2 - 50;
 		ball.y = window.height - 130;
-		while (box41.y != 50) {
+		while (boxes2[1].y != 50) {
 			for (int i = 0; i < 18; i++) {
 				boxes2[i].y += 5;
 			}
@@ -234,7 +228,7 @@ void newLevel() {
 	}
 }
 
-std::string A = "score: " + std::to_string((int)score);
+std::string A = "score: " + std::to_string((float)ball.x);
 std::string B = "lives: " + std::to_string((int)lives);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
@@ -244,11 +238,14 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 	//UNREFERENCED_PARAMETER(hPrevInstance);
 	//UNREFERENCED_PARAMETER(lpCmdLine);
-	InitGame();
 	InitWindow();
+	InitGame();
 	setText();
 	ShowCursor(NULL);
-	while (not(GetAsyncKeyState('A')) && not(GetAsyncKeyState('D'))) {
+	/*while (not(GetAsyncKeyState('A')) && not(GetAsyncKeyState('D'))) {
+		ShowBitmap(window.context, 0, 0, window.width, window.height, hBack);
+		ShowBitmap(window.context, racket.x, racket.y, racket.width, racket.height, racket.hBitmap);
+		ShowBitmap(window.context, ball.x, ball.y, ball.width, ball.height, ball.hBitmap);
 		TextOutA(window.context, window.width / 2 - 100, window.height / 2, "Rumble in the Jungle", 20);
 		TextOutA(window.context, window.width / 2 - 105, window.height / 2 + 100, "Press A or D to begin", 21);
 		if (GetAsyncKeyState('A')) {
@@ -257,36 +254,38 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		else if (GetAsyncKeyState('D')) {
 			ball.dirx = 1;
 		}
-	}
+	}*/
 	while (not(GetAsyncKeyState(VK_ESCAPE))) {
-		TextOutA(window.context, 10, window.height - 100, A.c_str(), 9);
-		TextOutA(window.context, 10, window.height - 50, B.c_str(), 9);
 		ShowBitmap(window.context, 0, 0, window.width, window.height, hBack);
 		ShowBitmap(window.context, racket.x, racket.y, racket.width, racket.height, racket.hBitmap);
 		ShowBitmap(window.context, ball.x, ball.y, ball.width, ball.height, ball.hBitmap);
 		for (int i = 0; i < 21; i++) {
-			ShowBitmap(window.context, boxes1[i].x, boxes1[i].y, boxes1[i].width, boxes1[i].height, boxes1[i].hBitmap);
+			ShowBitmap(window.context, boxes1[i].x, boxes1[i].y, window.width / 8, window.width / 8, boxes1[i].hBitmap);
 		}
-		for (int i = 0; i < 18; i++) {
-			ShowBitmap(window.context, boxes2[i].x, boxes2[i].y, boxes2[i].width, boxes2[i].height, boxes2[i].hBitmap);
+		if (level == 2) {
+			for (int i = 0; i < 18; i++) {
+				ShowBitmap(window.context, boxes2[i].x, boxes2[i].y, window.width / 7, window.width / 8, boxes2[i].hBitmap);
+			}
 		}
-		coll();
-		ballMove();
-		newLevel();
-		if (GetAsyncKeyState('A')) {
+		TextOutA(window.context, 10, window.height - 100, A.c_str(), 9);
+		TextOutA(window.context, 10, window.height - 50, B.c_str(), 9);
+		//coll();
+		//ballMove();
+		//newLevel();
+		/*if (GetAsyncKeyState('A')) {
 			racket.x -= racket.speed;
 		}
 		else if (GetAsyncKeyState('D')) {
 			racket.x += racket.speed;
 		}
-		if (ball.x == window.height - ball.height) {
+		if (ball.x >= window.height - ball.height) {
 			lives -= 1;
 			ball.x = window.width / 2 - 50;
 			ball.y = window.height - 130;
 		}
 		if (lives == 0) {
 			break;
-		}
+		}*/
 		BitBlt(window.device_context, 0, 0, window.width, window.height, window.context, 0, 0, SRCCOPY);
 		Sleep(16);
 	}
